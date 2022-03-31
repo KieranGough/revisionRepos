d3a83a86f9c5fb3b691043ce4b1358734bf796e8
KieranGough <kieran.gough@student.tafesa.edu.au>
2022-03-31 21:14:58 +1030

    Changed the ticket price for the balcony seat

--- Ticketing/Ticketing/TicketPrice.cs
+++ Ticketing/Ticketing/TicketPrice.cs
@@ -14,7 +14,7 @@ namespace Ticketing
         private decimal mPrice;
 
         const decimal mdecBalcony = 32.5m;
-        const decimal mdecGeneral = 28.75m;
+        const decimal mdecGeneral = 25.0m;
         const decimal mdecBox = 62.0m;
         const decimal mdecDiscount = 5.0m; 
 
